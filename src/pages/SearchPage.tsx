5ead5e78d4aed1c56604712f7f9b0e97cd76edd7
ANKIT12019 <rajankit12019@gmail.com>
2025-08-25 05:04:05 +0530

    commit

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -9,7 +9,7 @@ import AdvertisementBanner from "@/components/AdvertisementBanner";
 import RestaurantAdvertisement from "@/components/RestaurantAdvertisement";
 import SpecialOffersSection from "@/components/SpecialOffersSection";
 import { Button } from "@/components/ui/button";
-import { ChevronDown, ChevronUp, Clock, X } from "lucide-react";
+import { ChevronDown, ChevronUp, Clock, X, ArrowUp } from "lucide-react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import LoadingSpinner from "@/components/LoadingSpinner";

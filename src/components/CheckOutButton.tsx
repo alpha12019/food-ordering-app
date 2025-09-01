a642718cf7c9543df3e915929daed38a4ab69aa7
ANKIT12019 <rajankit12019@gmail.com>
2025-09-02 02:09:08 +0530

    commit

--- src/components/CheckOutButton.tsx
+++ src/components/CheckOutButton.tsx
@@ -13,8 +13,8 @@ type Props={
 }
 
 const CheckOutButton = ({onCheckout,disabled,isLoading}:Props) => {
-  const {isAuthenticated,isLoading:isAuthLoading,loginWithRedirect}=useAuth0();
-  const {pathname}=useLocation();
+      const {isAuthenticated, isLoading: isAuthLoading, loginWithRedirect} = useAuth0();
+      const {pathname} = useLocation();
   const {currentUser,isLoading:isGetuserLoading}=useGetMyUser();
   const onLogin=async()=>{
     loginWithRedirect({

261ef2599a6a7040cf0e75b4845609c863b4f572
ANKIT12019 <rajankit12019@gmail.com>
2025-09-15 14:38:24 +0530

    commit

--- src/components/CheckOutButton.tsx
+++ src/components/CheckOutButton.tsx
@@ -32,15 +32,6 @@ const CheckOutButton = ({ onCheckout, disabled, isLoading }: Props) => {
         Login to check out
       </Button>
     )
-  if (!isAuthenticated) {
-    return (
-      <Button
-        className="bg-orange-500 hover:bg-orange-600 flex-1 text-sm sm:text-base md:text-lg px-4 sm:px-6 py-2 sm:py-3 transition-all duration-300 hover:scale-105"
-        onClick={onLogin}
-      >
-        Login to check out
-      </Button>
-    )
   }
   if (isAuthLoading || !currentUser || isLoading) {
     return <LoadingButton></LoadingButton>
@@ -55,7 +46,7 @@ const CheckOutButton = ({ onCheckout, disabled, isLoading }: Props) => {
           Go to checkout
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-w-[95vw] sm:max-w-[425px] md:min-w-[700px] w-full mx-4 sm:mx-auto">
+      <DialogContent className="max-w-[95vw] sm:max-w-[400px] md:min-w-[750px] w-full mx-4 sm:mx-auto">
         <UserProfileForm currentUser={currentUser} onSave={onCheckout} isLoading={isGetuserLoading} title="Confirm Delivery Details" buttonText="Continue to payment">
 
         </UserProfileForm>

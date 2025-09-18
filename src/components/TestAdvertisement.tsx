df1b88da3795f1a31577ab9cc9210e8d03d55a20
ANKIT12019 <rajankit12019@gmail.com>
2025-09-18 14:31:12 +0530

    commit

--- src/components/TestAdvertisement.tsx
+++ src/components/TestAdvertisement.tsx
@@ -20,25 +20,3 @@ const TestAdvertisement = () => {
 };
 
 export default TestAdvertisement; 
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-
-const TestAdvertisement = () => {
-  return (
-    <Card className="mx-4 p-6 bg-orange-100 border-orange-300">
-      <CardContent>
-        <h3 className="text-xl font-bold text-orange-600 mb-2">
-          🎉 Test Advertisement
-        </h3>
-        <p className="text-gray-700 mb-4">
-      This is a test advertisement to verify that components are rendering properly.
-        </p>
-        <Button className="bg-orange-500 hover:bg-orange-600 text-white">
-          Test Button
-        </Button>
-      </CardContent>
-    </Card>
-  );
-};
-
-export default TestAdvertisement; 

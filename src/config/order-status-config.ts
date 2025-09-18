570b5b63a6e13c2413ed903c9fafb7676312c69e
ANKIT12019 <rajankit12019@gmail.com>
2025-09-18 14:29:27 +0530

    commit

--- src/config/order-status-config.ts
+++ src/config/order-status-config.ts
@@ -14,3 +14,19 @@ export const ORDER_STATUS:OrderStatusInfo[]=[
     {label:"Out For Delivery",value:"outforDelivery",progressValue:75},
     {label:"Delivered",value:"delivered",progressValue:100},
 ]
+import { OrderStatus } from "@/types"
+
+type OrderStatusInfo={
+    label:string,
+    value:OrderStatus,
+    progressValue:number
+}
+
+
+export const ORDER_STATUS:OrderStatusInfo[]=[
+    {label:"Placed",value:"placed",progressValue:0},
+    {label:"Awaiting Restaurant Confirmation",value:"paid",progressValue:25},
+    {label:"In Progress",value:"inProgress",progressValue:50},
+    {label:"Out For Delivery",value:"outforDelivery",progressValue:75},
+    {label:"Delivered",value:"delivered",progressValue:100},
+]

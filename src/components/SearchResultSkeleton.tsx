27dcf06b1d4c9ba5bb278df595de845b077795a7
ANKIT12019 <rajankit12019@gmail.com>
2025-09-18 14:27:43 +0530

    commit

--- src/components/SearchResultSkeleton.tsx
+++ src/components/SearchResultSkeleton.tsx
@@ -17,6 +17,7 @@ const SearchResultSkeleton = () => {
             <div className="h-4 bg-gray-200 rounded w-16"></div>
             <div className="h-4 bg-gray-200 rounded w-20"></div>
             <div className="h-4 bg-gray-200 rounded w-14"></div>
+            <div className="h-4 bg-gray-200 rounded w-14"></div>
           </div>
 
           {/* Info skeleton */}
